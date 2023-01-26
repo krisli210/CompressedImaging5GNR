23ba162234f85e90f3f49ff8141db0d5e45123f1
Kris Li <krisli210@gmail.com>
2023-01-26 13:41:55 -0600

    Processing 4 variable data script

---
+++ process_4D_Data.m
@@ -0,0 +1,36 @@
+close all
+
+PdStructure = load("data\PdGridCut.mat");
+PfStructure = load("data\PfGridCut.mat");
+
+PdGrid = PdStructure.PdGrid;
+PfGrid = PfStructure.PfGrid;
+
+KRange = PdStructure.KRangeCut;
+PdGrid = PdGrid(1:length(KRange), :, :, :);
+PfGrid = PfGrid(1:length(KRange), :, :, :);
+
+packetRange = PdStructure.packetRange;
+userRange = PdStructure.userRange;
+NTargetRange = PdStructure.NTargetRange;
+
+KStr = string([repmat('K = ', length(KRange), 1), num2str(KRange.')]);
+packetString = string([repmat('T = ', length(packetRange), 1), num2str(packetRange.')]);
+userString = string([repmat('U = ', length(userRange), 1), num2str(userRange.')]);
+NTargetString = string([repmat('N = ', length(NTargetRange), 1), num2str(NTargetRange.')]);
+
+figure; hold on; grid on;
+
+KFixed = 272; KIndex = find(KRange == KFixed);
+packetsFixed = 9; packetIndex = find(packetRange == packetsFixed);
+usersFixed= 3; userIndex = find(userRange == usersFixed);
+NTargetsFixed = 9; NTargetsIndex = find(NTargetRange == NTargetsFixed);
+
+yyaxis left;
+plot(NTargetRange, squeeze(PdGrid(KIndex, packetIndex, userIndex, :)), '-x');
+ylim([0 1]);
+
+yyaxis right;
+plot(NTargetRange, squeeze(PfGrid(KIndex, packetIndex, userIndex, :)), '-x');
+ylim([0 1]);
+
